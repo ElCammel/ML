16fe2f97abbf38a4392f2e987a8467778bf1c123
ElCammel <lamygregoire1@gmail.com>
2020-05-11 12:37:39 +0200

    unity scripts

---
+++ Scripts_Unity/CLionLibWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class CLionLibWrapper : MonoBehaviour
+{
+    [DllImport("ML")]
+    public extern static int my_add(int x, int y);
+
+    [DllImport("ML")]
+    public extern static int my_mult(int x, int y);
+}
---
+++ Scripts_Unity/TestCLionLibWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCLionLibWrapper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("With CLion Dll : ");
+        Debug.Log(CLionLibWrapper.my_add(42, 51));
+        Debug.Log(CLionLibWrapper.my_mult(2, 3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

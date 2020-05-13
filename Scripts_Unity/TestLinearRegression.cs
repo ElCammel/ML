cf48a8a9811ad45b98b954d3c550d7885474d60c
ElCammel <lamygregoire1@gmail.com>
2020-05-13 12:04:00 +0200

    update

---
+++ Scripts_Unity/TestLinearRegression.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestLinearRegression : MonoBehaviour
+{
+
+    public Transform[] trainingSpheres;
+    public Transform[] testSpheres;
+
+    public double[] trainingInputs;
+    public double[] trainingExpectedOutputs;
+
+    public int epoch = 1000;
+    public IntPtr? model;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateModel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateModel()
+    {
+        this.model = LinearRegression.linear_model_create(7);
+        Debug.Log("Created model");
+    }
+
+    public void Reinitialize()
+    {
+        // Put test spheres to 0
+        foreach (var testSphere in testSpheres)
+        {
+            var position = testSphere.position;
+            testSphere.position = new Vector3(position.x, 0, position.z);
+        }
+    }
+
+
+    public void Train()
+    {
+
+        if (this.model == null)
+        {
+            Debug.Log("Create model before");
+            return;
+        }
+
+        // Call lib to train on the array
+        var trainingSphereNumber = trainingSpheres.Length;
+        var trainingInputs = new double[trainingSphereNumber * 2];
+        var trainingExpectedInputs = new double[trainingSphereNumber];
+
+        for (var i = 0; i < trainingSphereNumber; i++)
+        {
+            trainingInputs[i * 2] = trainingSpheres[i].position.x;
+            trainingInputs[i * 2 + 1] = trainingSpheres[i].position.z;
+            trainingExpectedInputs[i] = trainingSpheres[i].position.y;
+        }
+        LinearRegression.linear_model_train_classification(this.model.Value, 2, epoch, 0.1, trainingInputs, trainingSphereNumber, trainingExpectedInputs);
+        Debug.Log("Model trained !");
+    }
+
+    public void Predict()
+    {
+
+        if (this.model == null)
+        {
+            Debug.Log("Create model before");
+            return;
+        }
+
+        // Call lib to predict test spheres
+        foreach (var testSphere in testSpheres)
+        {
+            var position = testSphere.position;
+            double[] paramsDim = { position.x, position.z };
+            var predicted = LinearRegression.linear_model_predict_classification(this.model.Value, 2, paramsDim);
+
+            position = new Vector3(
+                position.x,
+                predicted * (float)0.5,
+                position.z
+            );
+            testSphere.position = position;
+        }
+
+        Debug.Log("Predicted");
+    }
+}
--- library.cpp
+++ library.cpp
@@ -1,6 +1,8 @@
 #include "library.h"
-
+#include <Eigen>
+#include <random>
 #include <iostream>
+
 #if _WIN32
 #define DLLEXPORT __declspec
 #else#defined DLLEXPORT
@@ -8,12 +10,75 @@
 
 extern "C"{
 
-    __declspec(dllexport) int my_add(int x, int y){
-        return x + y;
+    __declspec(dllexport) int sign(double value) {
+        if(value == 0){
+            return 0;
+        }
+        else{
+            if( value < 0){
+                return -1;
+            }
+            else{
+                return 1;
+            }
+        }
     }
 
-    __declspec(dllexport) int my_mult(int x, int y){
-        return x * y;
+    __declspec(dllexport) double* linear_model_create(int input_dim){
+        auto ndArray = new double[input_dim + 1];
+
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_real_distribution<float> dist(-1, 1);
+
+        for(int i =0; i < input_dim + 1; i++){
+            ndArray[i] = dist(mt);
+        }
+
+        return ndArray;
+    }
+
+    __declspec(dllexport) double linear_model_predict_regression( double* model, int nb_dimension, double* inputs){
+        double sum = model[0];
+
+        for(int i = 0; i < nb_dimension; i++) {
+            sum += inputs[i] * model[i + 1];
+        }
+
+        return sum;
+    }
+
+    __declspec(dllexport) double linear_model_predict_classification(double* model, int nb_dimension, double* inputs){
+        double sum = model[0];
+
+        for(int i = 0; i < nb_dimension; i++) {
+            sum += inputs[i] * model[i + 1];
+        }
+
+        int result = sign(sum);
+
+        return result;
+    }
+
+
+    __declspec(dllexport) void linear_model_train_classification(double* model, int nb_dimension, int epoch, double steps,
+                                                                double* training_inputs, int training_inputs_number, double* training_expected_inputs){
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution<int> dist(0, 1);
+
+        for(int e = 0; e < epoch; e++) {
+            int trainingInput = (int)(dist(mt) * training_inputs_number);
+            int trainingParamsPosition = nb_dimension * trainingInput;
+            double modification = (double)steps * (training_expected_inputs[trainingInput] -
+                                    linear_model_predict_classification(model, nb_dimension, &training_inputs[trainingParamsPosition]));
+
+            model[0] += modification;
+
+            for(int j = 0; j < nb_dimension; j++) {
+                model[j + 1] += modification * training_inputs[trainingParamsPosition + j];
+            }
+        }
     }
 }
 
